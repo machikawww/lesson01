5d1af2d77fc505c201f6a65e7641a3147b574350
藤澤知可 <>
2020-06-27 16:51:52 +0900

    4-8 探索　を実装しました。

--- 4-8/main.js
+++ 4-8/main.js
@@ -1,1 +1,21 @@
 const str = "こはるはるここはるここはるはるここはるこはるはるはる"
+
+function SearchCount(text, name) {
+    let index = 0
+    let count = 0
+    for (; true;) {
+        index = text.indexOf(name, index)
+        if (index === -1) {
+            break;
+        }
+        index++
+        count++
+    }
+    return count
+}
+
+const a = 'こはる'
+const b = 'はるこ'
+const result1 = SearchCount(str, a)
+const result2 = SearchCount(str, b)
+console.log(`${a}:${result1}\n${b}:${result2}`)
