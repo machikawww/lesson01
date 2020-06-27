d441af11b5475bfee4ecb9f21091f1965fdf8a5a
藤澤知可 <>
2020-06-28 01:06:00 +0900

    4-6　少し修正しました。レビューありがとうございます

--- 4-6/main.js
+++ 4-6/main.js
@@ -4,16 +4,16 @@ const a = argv.split(':')
 const h = Number(a[0])
 const m = Number(a[1])
 
-function Clockplus(hour, minute) {
+function clockPlus(hour, minute) {
     minute += 45
     if (minute >= 60) {
         hour++
         minute -= 60
         if (hour >= 24) {
-            hour -= 24
+            hour %= 24
         }
     }
     return `${hour}:${minute}`
 }
 
-console.log(Clockplus(h, m))
+console.log(clockPlus(h, m))
