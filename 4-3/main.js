789ad20e0c76f8c1b6dd801552ee7e892ab79dc7
藤澤知可 <>
2020-06-27 16:44:14 +0900

    4-3　エラーコードを抽出を実装しました。

--- 4-3/main.js
+++ 4-3/main.js
@@ -3,3 +3,15 @@ const errorLogs = [
   "Error 3:Invalid Exception BBBB - 2020/01/01/02:00",
   "Error 22:Invalid Exception CCCCC - 2020/01/01/03:00"
 ]
+
+//console.log(errorLogs)
+
+for (let i = 0; i < errorLogs.length; i++) {
+  const arr = []
+  const error = errorLogs[i].split(/[:,\s,-]/)
+  //：と空白とーで区切った配列にする
+  for (let j = 2; j <= 4; j++) {
+    arr.push(error[j])
+  }
+  console.log(arr.join(' '))
+}
